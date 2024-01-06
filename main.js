2edeb17415d5969e69d7de6e8fc21dd42b76133c
Ho Nyeong <dev.honing@gmail.com>
2024-01-06 20:44:52 +0900

    Feat: MariaDB를 연결하면 JOIN DB를 생성하는 로직 작성

--- main.js
+++ main.js
@@ -8,4 +8,26 @@ const connection = mysql.createConnection({
   password: '1234',
 });
 
+
+// JOIN DB를 생성하는 쿼리문
+const createDatabaseQuery = `
+  CREATE DATABASE IF NOT EXISTS JOIN
+  `;
+
+// MariaDB에 연결하면 JOIN DB를 생성 
+connection.connect((error) => {
+  if (error) {
+    console.error('DB 연결 오류: ', error.message);
+  } else {
+    console.log('DB 연결 성공');
+  }
+  connection.query(createDatabaseQuery, (dbError) => {
+    if (dbError) {
+      console.error('DB 생성 오류: ', dbError.message);
+    } else {
+      console.log('DB 생성 성공 또는 이미 존재함');
+    }
+  })
+})
+
 // TABLE 생성
