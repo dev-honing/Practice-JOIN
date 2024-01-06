5b1b6d5ea908805e0331da2da67ed8938a8c3688
Ho Nyeong <dev.honing@gmail.com>
2024-01-06 20:57:51 +0900

    Chore: Prettier를 이용한 코드 스타일 정리

--- main.js
+++ main.js
@@ -1,44 +1,43 @@
 // main.js
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // MariaDB 연결 설정
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
+  host: "localhost",
+  user: "root",
+  password: "1234",
 });
-
 
 // TEST DB를 생성하는 쿼리문
 const createDatabaseQuery = `
   CREATE DATABASE IF NOT EXISTS TEST
   `;
 
-// MariaDB에 연결하면 TEST DB를 생성 
+// MariaDB에 연결하면 TEST DB를 생성
 connection.connect((error) => {
   if (error) {
-    console.error('DB 연결 오류: ', error.message);
+    console.error("DB 연결 오류: ", error.message);
   } else {
-    console.log('DB 연결 성공');
+    console.log("DB 연결 성공");
   }
 
   connection.query(createDatabaseQuery, (dbError) => {
     if (dbError) {
-      console.error('DB 생성 오류: ', dbError.message);
+      console.error("DB 생성 오류: ", dbError.message);
     } else {
-      console.log('DB 생성 성공 또는 이미 존재함');
+      console.log("DB 생성 성공 또는 이미 존재함");
     }
-  })
-})
+  });
+});
 
 // TABLE을 생성하는 쿼리문
 //* Galaxy Tab 사용자
 const createGalaxyTabTable = `
   CREATE TABLE galaxy_tab_owners (
-  user_id INT PRIMARY KEY,
-  user_name VARCHAR(255),
-  galaxy_tab_model VARCHAR(255)
-);
+    user_id INT PRIMARY KEY,
+    user_name VARCHAR(255),
+    galaxy_tab_model VARCHAR(255)
+  );
 `;
 
 //* Ipad 사용자
